59b98e0ba038584b4cc2bf114e00b91b61b96772
mkaufman385 <mkaufman385@gmail.com>
2025-04-18 16:50:59 -0400

    vite, tailwind, postCSS running properly

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <div className="min-h-screen bg-gray-100 p-4">
+        <h1 className="text-3xl font-bold text-center">Hello, Tailwind!</h1>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
